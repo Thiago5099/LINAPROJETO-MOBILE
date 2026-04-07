b4a7fea8439d3f81d17a8194ee00d5f654088231
Feby Kouyoumdjian Reis <frekore@gmail.com>
2026-04-07 20:28:45 -0300

    Update 1.0.5

--- app/src/main/java/com/example/projeto/LoginCadastro.java
+++ app/src/main/java/com/example/projeto/LoginCadastro.java
@@ -1,10 +1,13 @@
 package com.example.projeto;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,101 +29,57 @@ public class LoginCadastro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_cadastro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutLogin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Referências
+        LinearLayout layoutLogin = findViewById(R.id.layoutLogin);
+        LinearLayout layoutCadastro = findViewById(R.id.layoutCadastro);
+        Button btnEntrarTab = findViewById(R.id.buttonEntrarTab);
+        Button btnRegistrarTab = findViewById(R.id.buttonRegistrarTab);
 
-       /* layoutLogin = findViewById(R.id.layoutLogin);
-        layoutCadastro = findViewById(R.id.layoutCadastro);
-        btnLoginTab = findViewById(R.id.buttonLoginTab);
-        btnCadastroTab = findViewById(R.id.buttonCadastroTab);
+        Button btnEntrar = findViewById(R.id.buttonEntrar);
+        Button btnRegistrar = findViewById(R.id.buttonRegistrar);
 
-        btnLoginTab.setOnClickListener(v -> {
+        EditText editTextLoginEmail = findViewById(R.id.editTextLoginEmail);
+        EditText editTextLoginSenha = findViewById(R.id.editTextLoginSenha);
+        EditText editTextNome = findViewById(R.id.editTextNome);
+        EditText editTextCadastroEmail = findViewById(R.id.editTextCadastroEmail);
+        EditText editTextCadastroSenha = findViewById(R.id.editTextCadastroSenha);
+        CheckBox checkBoxDiabetico = findViewById(R.id.checkBoxDiabetico);
+
+        // Alternar abas
+        btnEntrarTab.setOnClickListener(v -> {
             layoutLogin.setVisibility(View.VISIBLE);
             layoutCadastro.setVisibility(View.GONE);
+            btnEntrarTab.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            btnRegistrarTab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#CBCBCB")));
         });
 
-        btnCadastroTab.setOnClickListener(v -> {
+        btnRegistrarTab.setOnClickListener(v -> {
             layoutCadastro.setVisibility(View.VISIBLE);
             layoutLogin.setVisibility(View.GONE);
-        });*/
+            btnRegistrarTab.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            btnEntrarTab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#CBCBCB")));
+        });
 
+        // Botões de ação
+        btnEntrar.setOnClickListener(v -> {
+            String email = editTextLoginEmail.getText().toString();
+            String senha = editTextLoginSenha.getText().toString();
+            Toast.makeText(getApplicationContext(), "Login com: " + email, Toast.LENGTH_SHORT).show();
+        });
 
-
-///Text View
-        ///Log.d("CicloDeVida", "onCreate() chamado");
-        ///TextView textViewMensagem = findViewById(R.id.textViewMensagem);
-        ///textViewMensagem.setText("Novo texto exibido!");
-///Text Edit
-        ///EditText editText1 = findViewById(R.id.editTextLoginEmail);
-        ///editText.setHint("Digite o seu e-mail!");
-        ///EditText editText2 = findViewById(R.id.editTextLoginSenha);
-        ///editText.setHint("Digite o sua senha!");
-        ///EditText editText3 = findViewById(R.id.editTextNome);
-        ///editText.setHint("Digite o seu nome!");
-        ///EditText editText4 = findViewById(R.id.editTextCadastroEmail);
-        ///editText.setHint("Digite o seu e-mail!");
-        ///EditText editText5 = findViewById(R.id.editTextCadastroSenha);
-        ///editText.setHint("Digite o sua senha!");
-///Botão dinâminco
-        ///Button buttonEntrar = findViewById(R.id.buttonLogar);
-        ///buttonEntrar.setOnClickListener(v -> {
-            ///Toast.makeText(getApplicationContext(), "Você entrou!",
-                    ///Toast.LENGTH_SHORT).show();
-        ///});
-        ///Button buttonResgistrar = findViewById(R.id.buttonRegistrar);
-        ///buttonRegistrar.setOnClickListener(v -> {
-            ///Toast.makeText(getApplicationContext(), "Você se registrou!",
-                    ///Toast.LENGTH_SHORT).show();
-        ///});
-///ImageView
-/// ImageView imageViewLogo = findViewById(R.id.imageViewLogo);
-/// imageViewLogo.setImageResource(R.drawable.nova_imagem);
+        btnRegistrar.setOnClickListener(v -> {
+            String nome = editTextNome.getText().toString();
+            String email = editTextCadastroEmail.getText().toString();
+            String senha = editTextCadastroSenha.getText().toString();
+            boolean diabetico = checkBoxDiabetico.isChecked();
+            Toast.makeText(getApplicationContext(), "Registrado: " + nome + " (" + email + ")", Toast.LENGTH_SHORT).show();
+        });
 
     }
-///Todos os modos
-    /*@Override
-    protected void onStart(){
-        super.onStart();
-        Log.d("CicloDeVida", "onStart() chamado");
-    }
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("CicloDeVida", "onRestart() chamado");
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d("CicloDeVida", "onResume() chamado");
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("CicloDeVida", "onPause() chamado");
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d("CicloDeVida", "onStop() chamado");
-    }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("CicloDeVida", "onDestroy() chamado");
-    }*/
-///Botão
-    ///public void logar(View view)
-    ///{
-        ///Intent intent = new Intent(MainActivity.this, NovaActivity.class);
-        ///startActivity(intent); ///Esse método abre uma nova activity
-    ///}
-///Botão
-    ///public void registrar(View view)
-    ///{
-        ///Intent intent = new Intent(MainActivity.this, NovaActivity.class);
-        ///startActivity(intent); ///Esse método abre uma nova activity
-    ///}
 }
