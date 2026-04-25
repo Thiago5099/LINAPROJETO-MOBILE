9ae40e906329b337dfeead0d4c0fdc9db22370b4
Thiago5099 <tv.thiagovalenca@gmail.com>
2026-04-24 21:14:37 -0300

    cabeçalho concluido

--- app/src/main/java/com/example/projeto/MainActivity.java
+++ app/src/main/java/com/example/projeto/MainActivity.java
@@ -28,9 +28,26 @@ public class MainActivity extends AppCompatActivity {
             banco.inserir
                     ("Maria Silva", "Infantil", "Santos", "(13) 99876-5432",
                             "maria@gmail.com", "200 + pacientes antendidos",3.0f);
+
+            banco.inserir("João Santos", "Clínica", "São Paulo",
+                    "(11) 91234-5678","joao@gmail.com", "100 + pacientes antendidos", 4.5f);
+
+            banco.inserir("Ma0ria Oliveira", "Infantil", "Campinas",
+                    "(19) 98765-4321","maria@gmail.com", "50 + pacientes antendidos", 3.8f);
+
+            banco.inserir("Pedro Almeida", "Clínica", "Santos",
+                    "(13) 99876-5432","pedro@gmail.com", "200 + pacientes antendidos", 4.2f);
+
+            banco.inserir("Ana Pereira", "Esportiva", "São Paulo",
+                    "(11) 91234-5678","ana@gmail.com", "150 + pacientes antendidos", 5.0f);
+
+            banco.inserir("Carlos Silva", "Clínica", "Campinas",
+                    "(19) 98765-4321","carlos@gmail.com", "50 + pacientes antendidos", 4.0f);
+
+            banco.inserir("Maria Santos", "Infantil", "Santos",
+                    "(13) 99876-5432","maria@gmail.com", "200 + pacientes antendidos", 3.0f);
+
         }
-
-
         ArrayList<Nutricionista> lista = banco.listarTodos();
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
