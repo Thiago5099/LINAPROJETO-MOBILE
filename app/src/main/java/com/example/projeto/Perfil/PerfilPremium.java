1ee76c00b608d00481ab248ca814f2a7e9a37c24
Thiago5099 <tv.thiagovalenca@gmail.com>
2026-04-29 21:40:54 -0300

    correção

--- app/src/main/java/com/example/projeto/Perfil/PerfilPremium.java
+++ app/src/main/java/com/example/projeto/Perfil/PerfilPremium.java
@@ -1,4 +1,4 @@
-package com.example.perfil;
+package com.example.projeto.Perfil;
 
 import android.os.Bundle;
 
@@ -10,16 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.projeto.R;
 
-public class MainActivity extends AppCompatActivity {
+public class PerfilPremium extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_perfil_premium);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
