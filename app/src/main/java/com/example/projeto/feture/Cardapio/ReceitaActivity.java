de8bd089c1d2224f35a72e7f5c52aa9f6d0e3f8f
Feby Kouyoumdjian Reis <frekore@gmail.com>
2026-05-01 17:25:30 -0300

    Update 04

--- app/src/main/java/com/example/projeto/feture/Cardapio/ReceitaActivity.java
+++ app/src/main/java/com/example/projeto/feture/Cardapio/ReceitaActivity.java
@@ -13,8 +13,14 @@ public class ReceitaActivity extends AppCompatActivity {
         super.onCreate(b);
         setContentView(R.layout.activity_cardapio_receita);
 
-        ((TextView)findViewById(R.id.nome)).setText(getIntent().getStringExtra("nome"));
-        ((TextView)findViewById(R.id.ingredientes)).setText(getIntent().getStringExtra("ingredientes"));
-        ((TextView)findViewById(R.id.preparo)).setText(getIntent().getStringExtra("preparo"));
+        ((TextView)findViewById(R.id.textReceitaMomento)).setText(getIntent().getStringExtra("Refeição do momento"));
+        ((TextView)findViewById(R.id.textTituloReceita)).setText(getIntent().getStringExtra("Título da receita"));
+        ((TextView)findViewById(R.id.textTagTempo)).setText(getIntent().getStringExtra("Tag(Tempo de preparo)"));
+        ((TextView)findViewById(R.id.textTagKcal)).setText(getIntent().getStringExtra("Tag(kcal)"));
+        ((TextView)findViewById(R.id.textTagSemGluten)).setText(getIntent().getStringExtra("Sem Glúten"));
+        ((TextView)findViewById(R.id.textTagSemLactose)).setText(getIntent().getStringExtra("Sem Lactose"));
+        ((TextView)findViewById(R.id.textIngredientes)).setText(getIntent().getStringExtra("Ingredientes"));
+        ((TextView)findViewById(R.id.textModoDePreparo)).setText(getIntent().getStringExtra("Modo de preparo"));
+        ((TextView)findViewById(R.id.textInformacoesNutricionais)).setText(getIntent().getStringExtra("Informações nutricionais"));
     }
 }
